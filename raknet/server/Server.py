cfe53fb5f90cd09fa6580d6a6bf3d9577c77deed
MFDGaming <38327951+MFDGaming@users.noreply.github.com>
2020-11-12 07:08:30 +0200

    Update Server.py

--- raknet/server/Server.py
+++ raknet/server/Server.py
@@ -4,8 +4,17 @@ class Server:
     socket = None
     
     def __init__(self, address, interface):
-        self.socket = socket.socket(socket.AF_INET if address.getVersion() == 4 else socket.AF_INET6, socket.SOCK_DGRAM)
-        
+        self.socket = socket.socket(socket.AF_INET if address.version == 4 else socket.AF_INET6, socket.SOCK_DGRAM socket.IPPROTO_UDP)
+        if address.getVersion() == 6:
+            self.socket.setsockopt(socket.IPPROTO_IPV6, socket.IPV6_V6ONLY, 1)
+        try:
+            self.socket.bind(address.ip, address.port)
+        except socket.error as e:
+            print(f"Failed to bind to to {str(address.port)}. Is a server already running on this port?")
+            print(str(e))
+            return
+        self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
         
     def sendPacket(self, packet, ip, port):
         packet.encode()
