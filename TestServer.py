da98783dd375861cb4957f12cda9edcc2fc30f1b
MFDGaming <38327951+MFDGaming@users.noreply.github.com>
2020-11-15 00:11:43 +0200

    Update TestServer.py

--- TestServer.py
+++ TestServer.py
@@ -3,11 +3,7 @@ from raknet.utils.InternetAddress import InternetAddress
 
 address = InternetAddress("0.0.0.0", 19132, 4)
 
-while True:
-    command = input()
-    if command == "":
-        pass
-    elif command == "stop":
-        exit()
-    else:
-        print("Invalid Command!")
+server = Server(address)
+
+server.setOption("name", "MCPE;Dedicated Server;390;1.14.60;0;10;13253860892328930865;Bedrock level;Survival;1;19132;19133;")
+server.startServer()
