aaa643a0056ae5fea089e9c6ab1814994fc5fb50
MFDGaming <38327951+MFDGaming@users.noreply.github.com>
2020-11-15 00:16:14 +0200

    Update Server.py

--- raknet/server/Server.py
+++ raknet/server/Server.py
@@ -6,14 +6,14 @@ from .Handler import Handler
 class Server(Thread):
     socket = None
     
-    def __init__(self, address, interface):
+    def __init__(self, address, interface = None):
         super().__init__()
         GeneralVariables.server = self
         self.socket = socket.socket(socket.AF_INET if address.version == 4 else socket.AF_INET6, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
         if address.version == 6:
             self.socket.setsockopt(socket.IPPROTO_IPV6, socket.IPV6_V6ONLY, 1)
         try:
-            self.socket.bind(address.ip, address.port)
+            self.socket.bind((address.ip, address.port))
         except socket.error as e:
             print(f"Failed to bind to to {str(address.port)}. Is a server already running on this port?")
             print(str(e))
@@ -33,6 +33,6 @@ class Server(Thread):
 
     def run(self):
         while True:
-            recv = socket.recvfrom(65535)
+            recv = self.socket.recvfrom(65535)
             if recv:
                 Handler.handle(recv[0], recv[1])
