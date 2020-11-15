95b7ff40ca7a99af816e50e3b99bc6a4c0f87a26
MFDGaming <38327951+MFDGaming@users.noreply.github.com>
2020-11-16 00:33:34 +0200

    Update Handler.py

--- raknet/server/Handler.py
+++ raknet/server/Handler.py
@@ -1,4 +1,8 @@
 from ..GeneralVariables import GeneralVariables
+from ..protocol.OpenConnectionReply1 import OpenConnectionReply1
+from ..protocol.OpenConnectionReply2 import OpenConnectionReply2
+from ..protocol.OpenConnectionRequest1 import OpenConnectionRequest1
+from ..protocol.OpenConnectionRequest2 import OpenConnectionRequest2
 from ..protocol.UnconnectedPing import UnconnectedPing
 from ..protocol.UnconnectedPingOpenConnections import UnconnectedPingOpenConnections
 from ..protocol.UnconnectedPong import UnconnectedPong
@@ -24,6 +28,25 @@ class Handler:
         newPacket.serverName = GeneralVariables.options["name"]
         return newPacket
     
+    def handleOpenConnectionRequest1(self, data):
+        packet = OpenConnectionRequest1()
+        packet.decode()
+        newPacket = OpenConnectionReply1()
+        newPacket.serverGuid = GeneralVariables.options["guid"]
+        newPacket.useSecurity = 0
+        newPacket.mtuSize = packet.mtuSize
+        return newPacket
+    
+    def handleOpenConnectionRequest2(self, data, address):
+        packet = OpenConnectionRequest2()
+        packet.decode()
+        newPacket = OpenConnectionReply2()
+        newPacket.serverGuid = GeneralVariables.options["guid"]
+        newPacket.clientAddress = address
+        newPacket.mtuSize = packet.mtuSize
+        newPacket.useSecurity = 0
+        return newPacket
+    
     def handle(self, data, address):
         id = data[0]
         if id == GeneralVariables.packetIds["UnconnectedPing"]:
@@ -31,4 +54,7 @@ class Handler:
             GeneralVariables.server.sendPacket(newPacket, address[0], address[1])
         elif id == GeneralVariables.packetIds["UnconnectedPingOpenConnections"]:
             newPacket = self.handleUnconnectedPingOpenConnections(data)
+            GeneralVariables.server.sendPacket(newPacket, address[0], address[1])
+        elif id == GeneralVariables.packetIds["OpenConnectionRequest1"]:
+            newPacket = self.handleOpenConnectionRequest1(data)
             GeneralVariables.server.sendPacket(newPacket, address[0], address[1])
