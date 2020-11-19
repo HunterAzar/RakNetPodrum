064fef675c637e342391804e05eec2418a6a37e4
MFDGaming <38327951+MFDGaming@users.noreply.github.com>
2020-11-19 15:15:19 +0200

    Create DataPacket.py

---
+++ raknet/protocol/DataPacket.py
@@ -0,0 +1,29 @@
+from .EncapsulatedPacket import EncapsulatedPacket
+from ..GeneralVariables import GeneralVariables
+from .Packet import Packet
+
+class DataPacket(Packet):
+    id = GeneralVariables.bitFlags["Valid"] | 0
+    sequenceNumber = None
+    packets = []
+    
+    def encodePayload(self):
+        self.putLTriad(self.sequenceNumber)
+        for packet in self.packets:
+            if isinstance(packet, EncapsulatedPacket):
+                packet.encode()
+                self.put(packet.buffer)
+            elif isinstance(packet, bytes):
+                self.put(packet)
+        
+    def decodePayload(self):
+        self.sequenceNumber = self.getLTriad()
+        while not self.feof():
+            data = self.buffer[self.offset:]
+            if data == b"":
+                break
+            packet = EncapsulatedPacket()
+            packet.buffer = data
+            packet.decode()
+            self.packets.append(packet)
+            self.offset += len(data)
