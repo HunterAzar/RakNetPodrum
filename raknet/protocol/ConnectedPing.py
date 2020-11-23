cb88fb62bda6e9f046c46e2959697aad0a6d6b01
MFDGaming <38327951+MFDGaming@users.noreply.github.com>
2020-11-23 13:37:19 +0200

    Create ConnectedPing.py

---
+++ raknet/protocol/ConnectedPing.py
@@ -0,0 +1,12 @@
+from ..GeneralVariables import GeneralVariables
+from .Packet import Packet
+
+class ConnectedPing(Packet):
+    id = GeneralVariables.ids["ConnectedPing"]
+    time = None
+
+    def encodePayload(self):
+        self.putLong(self.time)
+        
+    def decodePayload(self):
+        self.time = self.getLong()
