8cf605675cf4b3f4f891afca9310cd5f7e314724
MFDGaming <38327951+MFDGaming@users.noreply.github.com>
2020-11-23 13:48:23 +0200

    Create ConnectedPong.py

---
+++ raknet/protocol/ConnectedPong.py
@@ -0,0 +1,15 @@
+from ..GeneralVariables import GeneralVariables
+from .Packet import Packet
+
+class ConnectedPong(Packet):
+    id = GeneralVariables.ids["ConnectedPong"]
+    requestTime = None
+    replyTime = None
+
+    def encodePayload(self):
+        self.putLong(self.requestTime)
+        self.putLong(self.replyTime)
+        
+    def decodePayload(self):
+        self.requestTime = self.getLong()
+        self.replyTime = self.getLong()
