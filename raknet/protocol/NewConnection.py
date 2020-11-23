5c9c849fe3a0642b38adb1733914f6e934d50318
MFDGaming <38327951+MFDGaming@users.noreply.github.com>
2020-11-23 09:37:53 +0200

    Create NewConnection.py

---
+++ raknet/protocol/NewConnection.py
@@ -0,0 +1,23 @@
+from ..GeneralVariables import GeneralVariables
+from .Packet import Packet
+
+class NewConnection(Packet):
+    id = GeneralVariables.ids["NewConnection"]
+    clientAddress = None
+    systemAddresses = []
+    requestTime = None
+    replyTime = None
+
+    def encodePayload(self):
+        self.putAddress(self.clientAddress)
+        for i in range(0, GeneralVariables.options["systemAddressesCount"]):
+            self.putAddress(self.systemAddresses[i])
+        self.putLong(self.requestTime)
+        self.putLong(self.replyTime)
+        
+    def decodePayload(self):
+        self.clientAddress = self.getAddress()
+        for i in range(0, GeneralVariables.options["systemAddressesCount"]):
+            self.systemAddresses.insert(i, self.getAddress())
+        self.requestTime = self.getLong()
+        self.replyTime = self.getLong()
