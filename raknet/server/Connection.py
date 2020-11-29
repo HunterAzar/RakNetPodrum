a8cc8fcec95b5d38b8602d867b1f6cf0ce13f33a
MFDGaming <38327951+MFDGaming@users.noreply.github.com>
2020-11-30 00:15:28 +0200

    Update Connection.py

--- raknet/server/Connection.py
+++ raknet/server/Connection.py
@@ -1,8 +1,16 @@
 from ..GeneralVariables import GeneralVariables
 
 class Connection:
+    address = None
+    mtuSize = None
+    status = None
+    channelIndex = None
+    
     def __init__(self, address, mtuSize):
-        pass # Todo
+        self.address = address
+        self.mtuSize = mtuSize
+        self.status = GeneralVariables.connectionStates["Connecting"]
+        self.channelIndex = [0]*32
 
     def close(self):
         pass # Todo
