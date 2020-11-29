6ac5d550779a827637c68b14d5f1a3e354052eca
MFDGaming <38327951+MFDGaming@users.noreply.github.com>
2020-11-30 00:32:07 +0200

    Update Connection.py

--- raknet/server/Connection.py
+++ raknet/server/Connection.py
@@ -1,4 +1,5 @@
 from ..GeneralVariables import GeneralVariables
+from ..protocol.DataPacket import DataPacket
 from time import time
 
 class Connection:
@@ -8,6 +9,23 @@ class Connection:
     channelIndex = None
     isAcive = False
     lastUpdateTime = None
+    ackQueue = []
+    nackQueue = []
+    recoveryQueue = {}
+    packetToSend = []
+    sendQueue = None
+    fragmentedPackets = {}
+    windowStart = -1
+    windowEnd = 2048
+    reliableWindowStart = 0
+    reliableWindowEnd = 2048
+    reliableWindow = {}
+    lastReliableIndex = -1
+    receivedWindow = []
+    lastSequenceNumber = -1
+    sendSequenceNumber = 0
+    reliableFrameIndex = 0
+    fragmentId = 0
     
     def __init__(self, address, mtuSize):
         self.address = address
@@ -15,6 +33,7 @@ class Connection:
         self.status = GeneralVariables.connectionStates["Connecting"]
         self.channelIndex = [0]*32
         self.lastUpdateTime = time()
+        self.sendQueue = DataPacket()
 
     def close(self):
         pass # Todo
