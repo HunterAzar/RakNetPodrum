659c40f81bc146f1f98acf9775b84ce0c1152ba3
MFDGaming <38327951+MFDGaming@users.noreply.github.com>
2020-11-30 00:18:03 +0200

    Update Connection.py

--- raknet/server/Connection.py
+++ raknet/server/Connection.py
@@ -1,16 +1,20 @@
 from ..GeneralVariables import GeneralVariables
+from time import time
 
 class Connection:
     address = None
     mtuSize = None
     status = None
     channelIndex = None
+    isAcive = False
+    lastUpdateTime = None
     
     def __init__(self, address, mtuSize):
         self.address = address
         self.mtuSize = mtuSize
         self.status = GeneralVariables.connectionStates["Connecting"]
         self.channelIndex = [0]*32
+        self.lastUpdateTime = time()
 
     def close(self):
         pass # Todo
