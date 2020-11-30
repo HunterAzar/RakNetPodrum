004ce456d653f1c34d9f5bba1e3d873b6971aa77
MFDGaming <38327951+MFDGaming@users.noreply.github.com>
2020-11-30 23:42:54 +0200

    Update Connection.py

--- raknet/server/Connection.py
+++ raknet/server/Connection.py
@@ -34,6 +34,31 @@ class Connection:
         self.channelIndex = [0]*32
         self.lastUpdateTime = time()
         self.sendQueue = DataPacket()
+        
+    def addToQueue(self, packet, flags = GeneralVariables.packetPriorities["Normal"]):
+        priority = flags & 0b1
+        if priority == GeneralVariables.packetPriorities["Immediate"]:
+            newPacket = DataPacket()
+            packet.sequenceNumber = self.sendSequenceNumber
+            self.sendSequenceNumber += 1
+            newPacket.packets.append(packet)
+            GeneralVariables.server.sendPacket(newPacket, self.address)
+            newPacket.sendTime = time()
+            self.recoveryQueue[newPacket.sequenceNumber, newPacket]
+            return
+        length = self.sendQueue.getTotalLength()
+        if length + packet.getTotalLength() > self.mtuSize:
+            self.sendPacketQueue()
+        self.sendQueue.packets.append(packet)
+        
+    def sendPacketQueue(self):
+        if self.sendQueue.packets.getTotalLength() > 0:
+            self.sendQueue.sequenceNumber = self.sendSequenceNumber
+            self.sendSequenceNumber += 1
+            GeneralVariables.server.sendPacket(self.sendQueue, self.address)
+            self.sendQueue.sendTime = time()
+            self.recoveryQueue[self.sendQueue.sequenceNumber] = self.sendQueue
+            self.sendQueue = DataPacket()
 
     def close(self):
         pass # Todo
