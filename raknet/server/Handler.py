ede0e4f5aba5d1526478651d273ceccbc7101b60
MFDGaming <38327951+MFDGaming@users.noreply.github.com>
2020-12-01 14:20:10 +0200

    Update Handler.py

--- raknet/server/Handler.py
+++ raknet/server/Handler.py
@@ -203,8 +203,8 @@ class Handler:
         id = data[0]
         if GeneralVariables.options["debug"]:
             print(GeneralVariables.packetNames[id])
-        if GeneralVariables.server.getConnection(address):
-            connection = GeneralVariables.server.getConnection(InternetAddress(address[0], address[1]))
+        connection = GeneralVariables.server.getConnection(InternetAddress(address[0], address[1]))
+        if connection:
             connection.receive(data)
             connection.update(time())
         elif id == GeneralVariables.packetIds["UnconnectedPing"]:
