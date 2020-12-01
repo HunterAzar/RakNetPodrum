c100b367b96a48fc6ca5b0960a3faaf548724ec6
MFDGaming <38327951+MFDGaming@users.noreply.github.com>
2020-12-01 18:27:16 +0200

    Update TestServer.py

--- TestServer.py
+++ TestServer.py
@@ -1,7 +1,12 @@
 from raknet.server.Server import Server
 from raknet.utils.InternetAddress import InternetAddress
+import socket
 
-server = Server(InternetAddress("0.0.0.0", 19132))
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.connect(("8.8.8.8", 80))
+ip = s.getsockname()[0]
+
+server = Server(InternetAddress(ip, 19132))
 
 server.setOption("name", "MCPE;Dedicated Server;390;1.14.60;0;10;13253860892328930865;Bedrock level;Survival;1;19132;19133;")
 server.startServer()
