3456c7fcd6f51aab8d7417fafce4a27861eab7cd
MFDGaming <38327951+MFDGaming@users.noreply.github.com>
2020-12-01 14:04:50 +0200

    Update ConnectionClosed.py

--- raknet/protocol/ConnectionClosed.py
+++ raknet/protocol/ConnectionClosed.py
@@ -2,4 +2,4 @@ from ..GeneralVariables import GeneralVariables
 from .Packet import Packet
 
 class ConnectionClosed(Packet):
-    id = GeneralVariables.ids["ConnectionClosed"]
+    id = GeneralVariables.packetIds["ConnectionClosed"]
